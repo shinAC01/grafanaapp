9e0ada08af4052206ee0f19777c0264b678bb833
Luisito <gricel.caballero04@gmail.com>
2023-11-06 09:53:04 -0500

    Titulo

---
+++ grafanaapp-master/src/main/java/cu/uci/grafanapp/GrafanappApplication.java
@@ -0,0 +1,13 @@
+package cu.uci.grafanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrafanappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrafanappApplication.class, args);
+	}
+	//Commit de prueba
+}
