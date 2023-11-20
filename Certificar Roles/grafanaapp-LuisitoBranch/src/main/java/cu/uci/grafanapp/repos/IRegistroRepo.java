b465f98d32d74f1d0729d1e1c5a5d1f22439263f
Luis Rodriguez <gricel.caballero04@gmail.com>
2023-11-20 10:21:54 -0500

    Push de prueba

--- Certificar Roles/grafanaapp-LuisitoBranch/src/main/java/cu/uci/grafanapp/repos/IRegistroRepo.java
+++
@@ -1,8 +0,0 @@
-package cu.uci.grafanapp.repos;
-
-import cu.uci.grafanapp.entity.Registro;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IRegistroRepo extends CrudRepository<Registro,Long> {
-
-}
